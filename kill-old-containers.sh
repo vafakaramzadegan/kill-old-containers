5bfab213f4ffdd1224d8d8e773ba2ab7fb12d73d
Vafa Karamzadegan <63124707+vafakaramzadegan@users.noreply.github.com>
2022-02-05 11:14:25 +0330

    Update kill-old-containers.sh

--- kill-old-containers.sh
+++ kill-old-containers.sh
@@ -1,76 +1,47 @@
 #!/bin/bash
 
-
-function convert_time_ago(){
-	# convert "time ago" values to seconds.
-	# example:
-	# 5 minutes ago => 300
-	# 2 hours ago => 7200
-	# 1 day ago => 86400
-	REGEX="([0-9]+) +([a-z]+)"
-	if [[ $1 =~ $REGEX ]]
-	then
-		VAL=${BASH_REMATCH[1]}
-		UNIT=${BASH_REMATCH[2]}
-	fi
-	case "$UNIT" in
-		"seconds" )
-			echo $VAL
-			;;
-		"minutes" )
-			echo $(($VAL*60))
-			;;
-		"hours" )
-			echo $(($VAL*3600))
-			;;
-		"days" )
-			echo $(($VAL*86400))
-			;;
-	esac
-}
+YELLOW='\033[1;33m'
+LIGHT_CYAN='\033[1;36m'
+# no color
+NC='\033[0m'
 
 function kill_old_containers(){
-	# regex to parse `$ docker containers list` output.
-	# ----------------------------------------------------
-	# | CONTAINER ID | ... |   CREATED     | ... | NAMES | 
-	# ----------------------------------------------------
-	# | ............ | ... | ([0-9]+.+ago) | ... | (.+)  |
-	# ----------------------------------------------------
-	CONTAINER_REGEX=" +([0-9]+.+ago).+ +(.+)"
-	# get containers information.
-	# we need to change the internal field separator to newline (\n)
-	# this helps converting each line of `$ docker container ls` output
-	# to an array item.
-	INITIAL_IFS=$IFS
+	# changing IFS to newline helps converting "$(docker ps)" output
+	# to array items
 	IFS=$'\n'
-	echo "Fetching the list of running containers..."
-	OUTPUT=($(docker container ls))
-	IFS=$INITIAL_IFS
-
-	echo "Terminating containers older than ${1} seconds:"
-	for (( 0; i<${#OUTPUT[@]}; i++ ))
+	# docker ps output: [CONTAINER ID] [CONTAINER NAME]
+	OUTPUT=($(docker ps --format "{{.ID}} {{.Names}}" | grep "${2}"))
+	printf "Killing containers older than ${1} seconds:\n\n"
+	CONT_COUNT=${#OUTPUT[@]}
+	if [ $CONT_COUNT == 0 ]; then
+		echo "No containers found."
+		exit 0
+	fi
+	NUM_KILLED=0
+	for (( 0; i<$CONT_COUNT; i++ ))
 	do
-		REC="${OUTPUT[$i]}"
-		if [[ $REC =~ $CONTAINER_REGEX ]]
+		REC=${OUTPUT[$i]}
+		# split content of each record by whitespace
+		CONT_ID=$(echo $REC | cut -d' ' -f1)
+		CONT_NAME=$(echo $REC | cut -d' ' -f2)
+		# convert the time the container started to unix timestamp
+		CONT_TIMESTAMP=$(docker inspect --format=\"{{.State.StartedAt}}\" $CONT_ID | xargs date +%s -d)
+		CURRENT_TIMESTAMP=$(date +%s)
+		DIFF=$(expr $CURRENT_TIMESTAMP - $CONT_TIMESTAMP)
+		if [[ $DIFF -ge $1 ]]
 		then
-			# BASH_REMATCH[1] contains a string with 3 segments:
-			# [integer number] [string (seconds, minutes, hours, days, ...)] ago
-			CREATED_TS=$(convert_time_ago ${BASH_REMATCH[1]})
-			CONT_NAME="${BASH_REMATCH[2]}"
-			# check if container is created before the requested
-			# number of seconds.
-			if [[ $CREATED_TS -ge $1 ]]
-			then
-				echo "Terminating ${CONT_NAME}..."
-				KILL_CMD="docker kill ${CONT_NAME}"
-				# invoke docker kill command in the background to prevent
-				# possible freezing.
-				eval "${KILL_CMD}" &>/dev/null & disown
-			fi
+			echo -e "${YELLOW}$CONT_ID${NC}($CONT_NAME): ${LIGHT_CYAN}$DIFF${NC} seconds ago"
+			KILL_CMD="docker kill ${CONT_ID}"
+			# invoke docker kill command in the background to prevent
+			# possible freezing
+			#eval "${KILL_CMD}" &>/dev/null & disown
+			let "NUM_KILLED+=1"
 		fi
 	done
+	if [[ $NUM_KILLED -gt 0 ]]; then
+		printf "\nkilled $NUM_KILLED containers."
+	fi
 }
-
 
 # check if Docker is installed
 if [ ! -x "$(command -v docker)" ]; then
@@ -78,4 +49,27 @@ if [ ! -x "$(command -v docker)" ]; then
 	exit 0
 fi
 
-echo "$(kill_old_containers $1)"
+SEARCH=""
+while [ "$1" != "" ]; do
+    case $1 in
+        -s | --search )
+			shift
+			SEARCH="$1";;
+		-t | --time )
+			shift
+			TIME=$1
+    esac
+    shift
+done
+
+if [[ "$TIME" -eq "$TIME" ]] 2>/dev/null ; then
+	if [[ $TIME -gt 0 ]]; then
+		echo "$(kill_old_containers $TIME $SEARCH)"
+	else
+		echo "Invalid value for the number of seconds!"
+		exit 0
+	fi
+else
+	echo "Invalid value for the number of seconds!"
+	exit 0
+fi
